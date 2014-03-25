0fbb9b7603040dca6cb56820179338ddaf3c4f7d
jheddings <jason@heddings.com>
2014-03-25 14:38:01 -0600

    Initial app framework.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,43 @@
-angular.module('pgcomp', [])
+angular.module('pgcomp.controllers', [])
 
+////////////////////////////////////////////////////////////////////////////////
 .controller('MapListCtrl', ['$scope', '$http',
   function ($scope, $http) {
     $http.get('maps/index.json').success(function(data) {
       $scope.maps = data;
     });
+
+    $scope.mapId = null;
+    $scope.numPlayers = 2;
+  }
+])
+
+////////////////////////////////////////////////////////////////////////////////
+.controller('QuickRefCtrl', ['$scope', '$routeParams', '$http',
+  function ($scope, $routeParams, $http) {
+    var mapId = $routeParams.mapId;
+
+    $http.get('maps/' + mapId + '.json').success(function(data) {
+      $scope.map = data;
+    });
+
+    $scope.mapId = mapId;
+  }
+])
+
+////////////////////////////////////////////////////////////////////////////////
+.controller('WalkthroughCtrl', ['$scope', '$routeParams', '$http',
+  function ($scope, $routeParams, $http) {
+    var mapId = $routeParams.mapId;
+    var numPlayers = $routeParams.numPlayers;
+    var step = $routeParams.step;
+
+    $http.get('maps/' + mapId + '.json').success(function(data) {
+      $scope.map = data;
+    });
+
+    $scope.mapId = mapId;
+    $scope.numPlayers = numPlayers;
+    $scope.step = numPlayers;
   }
 ]);
