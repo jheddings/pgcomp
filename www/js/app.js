9afe569968949b2a448d298c16dd446435ce11ad
jheddings <jason@heddings.com>
2014-03-24 20:15:59 -0600

    Cleaned up sample app...

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,11 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
+angular.module('pgcomp', ['ionic', 'pgcomp.services', 'pgcomp.controllers'])
 
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
-    // setup an abstract state for the tabs directive
     .state('tab', {
       url: "/tab",
       abstract: true,
@@ -24,7 +13,6 @@ angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
       controller: 'TabCtrl'
     })
 
-    // the pet tab has its own child nav-view and history
     .state('tab.home', {
       url: '/home',
       views: {
@@ -65,7 +53,6 @@ angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
       }
     });
 
-  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/home');
 
 });
